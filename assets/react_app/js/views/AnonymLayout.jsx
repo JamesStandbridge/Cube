394194846468e95bc66c9d4075bd55058ae1f47f
jamesstandbridge <james.standbridge.git@gmail.com>
2021-03-27 21:38:26 +0100

    wave style

--- assets/react_app/js/views/AnonymLayout.jsx
+++ assets/react_app/js/views/AnonymLayout.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styled from 'styled-components'
 import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader, SidebarContent, SidebarFooter } from 'react-pro-sidebar';
-import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+	
+import WaveSVG from '../../img/background-waves.svg'
 
 import { Link } from 'react-router-dom';
 
@@ -14,17 +15,15 @@ import 'react-pro-sidebar/dist/css/styles.css';
 
 const AnonymLayout = ({children}) => {
 
-
+	console.log(WaveSVG)
 	return (
-		<>
-			<div className="app-bar full">
-				<AppBar position="static">
-					<AppBarContent>
+		<div>
+			<AppBar className="app-bar full">
+				<AppBarContent>
 					<CubeTitle className="app-title">
 						CUBE
 					</CubeTitle>
-					<Toolbar style={{marginLeft: "auto"}}>
-						<div className="app-bar-actions">
+					<AppActions className="app-bar-actions">
 						<Link to="/login" replace>
 							<Button 
 								className="btn connect"
@@ -50,25 +49,44 @@ const AnonymLayout = ({children}) => {
 							Créer un compte
 							</Button>
 						</Link>
-						</div>
-					</Toolbar>
-					</AppBarContent>
-				</AppBar>
-			</div>
-		</>
+					</AppActions>
+
+				</AppBarContent>
+				<WabeBar>
+
+				</WabeBar>
+			</AppBar>
+		</div>
 	)
 }
 
 export default AnonymLayout;
 
+const AppBar = styled.div`
+	background-image: url(${WaveSVG});
+	background-image: rebeccapurple;
+    background-repeat: no-repeat;
+    background-size: 100% 42.555%;
+`
+
+const AppActions = styled.div`
+    margin-top: 50px;
+    margin-right: 50px;
+`
+
 const CubeTitle = styled.h1`
-	text-align: center;
-    font-size: 60px;
-    margin-top: 28px;
+    text-align: center;
+    font-size: 100px;
+    margin-top: 64px;
     margin-bottom: 28px;
-    margin-left: 46.7%;
+    margin-left: 43.7%;
+    color: white;
 `
 
 const AppBarContent = styled.div`
 	display: flex;
+`
+
+const WabeBar = styled.div`
+	
 `
